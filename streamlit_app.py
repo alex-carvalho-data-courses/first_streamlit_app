e38c2c58eff9a8f5cf940c08b4281c3bcafd7825
alex carvalho data <76410941+alex-carvalho-data@users.noreply.github.com>
2023-10-18 19:58:55 +0200

    fix typo in streamline

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build your own Fruit Smoothie')
-strealit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
