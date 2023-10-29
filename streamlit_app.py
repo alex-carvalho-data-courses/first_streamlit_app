d564e4f3a3375101797fc172375835591d5953d8
alex carvalho data <76410941+alex-carvalho-data@users.noreply.github.com>
2023-10-30 00:23:37 +0100

    close connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,7 @@ except URLError as e:
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
   my_data_rows = get_fruit_load_list(my_cnx)
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
